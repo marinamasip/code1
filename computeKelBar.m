e09b4fd41374d4e5755a01763ca7d151a455cfd6
marinamasip <113786930+marinamasip@users.noreply.github.com>
2022-09-20 17:13:35 +0200

    test1

--- computeKelBar.m
+++ computeKelBar.m
@@ -5,7 +5,7 @@ for e = 1 : n_el
     x1 = x(Tn(e,1),1);
     x2 = x(Tn(e,2),1);
     l = abs(x2-x1);
-    
+    r=(x1+x2)/l;
     K = (mat(Tmat(e),1)*mat(Tmat(e),3)./l) .* [12 6*l -12 6*l;
         6*l (4*l)^2 -6*l (2*l)^2;
         -12 -6*l 12 -6*l;
@@ -15,10 +15,7 @@ for e = 1 : n_el
     for r = 1 : (n_ne*n_i)
         for s = 1 : (n_ne*n_i)
             Kel(r,s,e) = K(r,s);
-        
+
         end
     end
 end
-
-    
-    
